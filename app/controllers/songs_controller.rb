b4cb42273d41c8e68c5fe3e8eed9fd081751b5f5
github username <github email address>
2020-11-01 12:41:17 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,10 @@
 require 'sinatra/base'
 require 'rack-flash'
 
-class SongsController < Sinatra::Base
+class SongsController < ApplicationController
   enable :sessions
   use Rack::Flash
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
-
+  
   get '/songs' do
     @songs = Song.all
     erb :"songs/index"
